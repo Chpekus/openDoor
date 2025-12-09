7a3473ab5fdd8f4ef28747938953f3c15fcca662
chpekus <mi.kalashnikov@yandex.ru>
2025-12-09 17:43:12 +0700

    separate code into function

---
+++ novotelecom_integrarion.py
@@ -0,0 +1,49 @@
+import requests
+
+def get_stream_url_via_requests(login, password):
+    session = requests.Session()
+
+    login_url = "https://video.2090000.ru/login.html"
+    payload = {
+        "User[Login]": login,
+        "User[Password]": password,
+    }
+    session.post(login_url, data=payload)
+
+    url = "https://video.2090000.ru/account/camera/3104703/url.html"
+    params = {
+        "speed_mul": 1,
+        "time": "",
+                
+        "timeZoneOffset": 25200,
+        "format": "hls",
+        "isSingleCamera": 1,
+        "_": "",
+    }
+
+    resp = session.get(url, params=params)
+    session.close()
+    # print(resp.status_code)
+    # print(resp.json()['URL'])
+    return resp.json()['URL']
+
+
+def send_post_open_door_request(bearer_token):
+    url = "https://myhome.proptech.ru/rest/v1/places/260209/accesscontrols/10586/entrances/1824/actions"
+
+    headers = {
+        "user-agent":       "samsung SMS908E | Android 9 | ntkSmartphoneGmsGooglePlay | 6.21.0 (7040302) | 65973 | 1 | 1da05087-058e-40a4-918b-e1a6675674b2",
+        "operator":         "1",
+        "authorization":    f"Bearer {bearer_token}", # Ключ получен в пятницу 5.12.2025
+        "content-type":    "application/json; charset=UTF-8",
+        "content-length":   "28",
+        "accept-encoding":  "gzip"
+    }
+
+    payload = {
+        "name": "accessControlOpen"
+    }
+
+    resp = requests.post(url, json=payload, headers=headers)
+    print(resp.status_code)
+    print(resp.text)
---
+++ recognition_alorithms.py
@@ -0,0 +1,55 @@
+def finger_is_open(wrist, tip, pip, coeff):
+    """
+    Считаем палец "открытым", расстояние от кисти до кончика пальца больше, 
+    чем (расстояние от кисти до первой фаланги пальца) * 1.45.
+    """
+
+
+    def powDist(a, b):
+        return ((a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y))
+    #return [int(powDist(wrist, pip) * coeff*coeff < powDist(wrist, tip)), str(powDist(wrist, pip) * coeff*coeff)[:4], str(powDist(wrist, tip))[:4]]
+    #print(powDist(wrist, pip) * coeff*coeff, powDist(wrist, tip))
+    return int(powDist(wrist, pip) * coeff*coeff < powDist(wrist, tip))
+
+
+def open_fingers(hand_landmarks):
+    """
+    Возвращает количество "открытых" пальцев для одной руки.
+    """
+    finger_tips = [4, 8, 12, 16, 20]
+    finger_pips = [2, 5, 9, 13, 17]
+    coeffForFingers = [1.5, 1.6, 1.8, 1.8, 1.6]
+
+    open_finger = []
+    
+    for tip_id, pip_id, coeff_id in zip(finger_tips, finger_pips, coeffForFingers):
+        open_finger.append(finger_is_open(hand_landmarks.landmark[0], hand_landmarks.landmark[tip_id], hand_landmarks.landmark[pip_id], coeff_id))
+
+    return open_finger
+
+
+def classify_gesture(hand_landmarks):
+    """
+    Классификация жеста по количеству открытых пальцев и их относительному расположению.
+    """
+    list_open_fingers = open_fingers(hand_landmarks)
+    #return f"{list_open_fingers}"
+
+    count_open_fingers = sum(list_open_fingers)
+    
+    if count_open_fingers == 0:
+        return "Fist"
+    elif count_open_fingers == 5:
+        return "Open Hand"
+    elif count_open_fingers == 1 and list_open_fingers[1] == 1:
+        return "Ukazannie"
+    elif count_open_fingers == 2 and list_open_fingers[1] == 1 and list_open_fingers[2] == 1: 
+        return "Victory"
+    elif count_open_fingers == 3 and list_open_fingers[0] == 1 and list_open_fingers[1] == 1 and list_open_fingers[2] == 1: 
+        return "TiDishi"
+    elif count_open_fingers == 2 and list_open_fingers[0] == 1 and list_open_fingers[4] == 1: 
+        return "Jambo"
+    elif count_open_fingers == 3 and list_open_fingers[0] == 1 and list_open_fingers[1] == 1 and list_open_fingers[4] == 1: 
+        return "Rock"
+    else:
+        return f"{list_open_fingers}: {count_open_fingers}"

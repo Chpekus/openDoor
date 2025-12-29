90c8ee9faa07c18465a76e13de23a1c75cf02b07
chpekus <mi.kalashnikov@yandex.ru>
2025-12-29 21:36:06 +0700

    add function to make session for web-site novotelecom

--- novotelecom_integrarion.py
+++ novotelecom_integrarion.py
@@ -1,18 +1,28 @@
 import requests
+import os
+from dotenv import load_dotenv
 
-def get_stream_url_via_requests(login = "", password = "", session = None):
-    
-    if session == None: # Будем создавать сессию при первой авторизации и использовать её при следющих запросах стрима
-        session = requests.Session()
+load_dotenv()
+login = os.getenv("LOGIN")     
+password = os.getenv("PASSWORD")
 
+def make_session():
+        session = requests.Session() # Создаем сессию на сайте новотелекома с камерами домофонов по логину, паролю из env
         login_url = "https://video.2090000.ru/login.html"
         payload = {
             "User[Login]": login,
             "User[Password]": password,
         }
         session.post(login_url, data=payload)
+        return session
 
-    url = "https://video.2090000.ru/account/camera/3104703/url.html" # Получаем ссылку на стрим камеры, пока по статичкому id домофона
+def get_stream_url(session = None, id_intercom = 0):
+    
+    if session == None: # Будем создавать сессию при первой авторизации и использовать её при следющих запросах стрима
+        session = make_session()
+
+    url = f"https://video.2090000.ru/account/camera/{id_intercom}/url.html" # Получаем ссылку на стрим камеры, по  id домофона
+    
     params = {
         "speed_mul": 1,
         "time": "",
@@ -21,13 +31,15 @@ def get_stream_url_via_requests(login = "", password = "", session = None):
         "isSingleCamera": 1,
         "_": "",
     }
-
-    resp = session.get(url, params=params)
-    # session.close()
-    # print(resp.status_code)
-    # print(resp.json())
-    return resp.json()['URL'], session # возвращаем ссылку на стрим и сессию
-
+    try:
+        resp = session.get(url, params=params)
+        URL_Stream = resp.json()['URL']
+    except Exception as e:
+        print(URL_Stream)
+        print(f"Error in novotelecom integration {e}")
+        return 0
+    
+    return URL_Stream, session
 
 def send_post_open_door_request(bearer_token):
     url = "https://myhome.proptech.ru/rest/v1/places/260209/accesscontrols/10586/entrances/1824/actions"
@@ -46,6 +58,5 @@ def send_post_open_door_request(bearer_token):
     }
 
     resp = requests.post(url, json=payload, headers=headers)
-    print(resp.status_code)
-    print(resp.text)
+
     return resp.status_code, resp.text
